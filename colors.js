26ef906a1999fe7a1d70b4bba3aebdfa642b39d3
SadiPro07 <saad.khan@zortik.com>
2025-01-01 19:03:02 +0500

    first commit

---
+++ colors.js
@@ -0,0 +1,9 @@
+const colors = {
+    primary: "#1E40AF", // Blue
+    secondary: "#9333EA", // Purple
+    accent: "#F59E0B", // Amber
+    background: "#F3F4F6", // Gray
+    text_gali: "#eafdba", // Dark Gray
+  };
+  
+  module.exports = colors;
