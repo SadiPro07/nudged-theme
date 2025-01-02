02ad0b57dc84ac36d8f4fa5f93e93584b17e4f19
SadiPro07 <saad.khan@zortik.com>
2025-01-02 08:25:04 +0500

    update fonts

---
+++ font.js
@@ -0,0 +1,16 @@
+export const Rubik_Bold = "Rubik-Bold";
+export const Rubik_Medium = "Rubik-Medium";
+export const Rubik_Regular = "Rubik-Regular";
+
+const fonts = {
+  roboto: {
+    regular: "fonts/Rubik-Regular.ttf",
+    bold: "fonts/Rubik-Bold.ttf",
+  },
+  openSans: {
+    regular: "fonts/OpenSans-Regular.ttf",
+    bold: "fonts/OpenSans-Bold.ttf",
+  },
+};
+
+module.exports = fonts;
--- fonts/index.js
+++
@@ -1,3 +0,0 @@
-export const Rubik_Bold = "Rubik-Bold";
-export const Rubik_Medium = "Rubik-Medium";
-export const Rubik_Regular = "Rubik-Regular";
