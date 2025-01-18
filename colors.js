48695f5a8b7a7c228d09820c4949e6b24b093d05
SadiPro07 <saad.khan@zortik.com>
2025-01-18 14:55:43 +0500

    update colors

--- colors.js
+++ colors.js
@@ -1,9 +1,30 @@
+// const colors = {
+//   primary: "#1E40AF", // Blue
+//   secondary: "#9333EA", // Purple
+//   accent: "#F59E0B", // Amber
+//   background: "#F3F4F6", // Gray
+//   text_gali: "#0d0a2f", // Dark Gray
+// };
+
+// module.exports = colors;
+
 const colors = {
-  primary: "#1E40AF", // Blue
-  secondary: "#9333EA", // Purple
-  accent: "#F59E0B", // Amber
-  background: "#F3F4F6", // Gray
-  text_gali: "#0d0a2f", // Dark Gray
+  primary: {
+    base: "#2E89E5", // Hoff Blue
+    hover: "#C0DCF7", // Hover Blue (menu)
+    background: "#EAF3FC", // Light Blue (BG)
+  },
+  secondary: {
+    base: "#333966", // Dark Blue
+    border: "#999CB3", // Border Grey
+  },
+  accent: {
+    success: "#44B102", // Green
+    warning: {
+      base: "#FFD4A6", // Blush
+      tint: "#FFE9D2", // Blush - tint 50
+    },
+  },
 };
 
 module.exports = colors;
